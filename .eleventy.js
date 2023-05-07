5ef9985a8550bc159ee0dc50c88d5922015345bb
Tobias Skarhed <tobias.skarhed@gmail.com>
2023-05-07 19:23:40 +0200

    Add styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,10 @@
 module.exports = function(eleventyConfig) {
 
+    eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
+
     // Copy any .svg or midi file to `_site`, via Glob pattern
     // Keeps the same directory structure
     eleventyConfig.addPassthroughCopy("songs/**/*.svg");
     eleventyConfig.addPassthroughCopy("songs/**/*.midi");
+    eleventyConfig.addPassthroughCopy("songs/styles.css");
 };
