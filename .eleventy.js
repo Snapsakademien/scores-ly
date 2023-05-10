2a3b1eda18d1faafdd1077305b97b9fed69cda49
Tobias Skarhed <tobias.skarhed@gmail.com>
2023-05-10 21:47:59 +0200

    Cleanup

--- .eleventy.js
+++ .eleventy.js
@@ -1,15 +1,6 @@
-// const yaml = require("js-yaml");
 
 module.exports = function(eleventyConfig) {
     eleventyConfig.setUseGitIgnore(false);
-
-    // eleventyConfig.addDataExtension("yaml", (contents, filePath) => {
-    //     console.log("Trying to process ", filePath);
-    //     return yaml.load(contents);
-    // });
-
-
-    //eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
 
     // Copy any .svg or midi file to `_site`, via Glob pattern
     // Keeps the same directory structure
